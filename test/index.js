d41aae74c7e3c22e723c7c1a9cfb57b86770d5c5
Zane Milakovic <zane.milakovic@outlook.com>
2015-12-04 20:47:03 -0600

    removed unneded files and fixed testing issues

--- test/index.js
+++ test/index.js
@@ -11,20 +11,18 @@ chai.config.truncateThreshold = 0; // disable truncating
 
 // Impress.js Tests
 describe('impress', function() {
+  
+  describe('construction of class', function() {
+    
+    it('should construct a instance of Impress with default options if no options are passed in', function() {
+        var impress = new Impress();
+        impress.should.have.deep.property('options');
+    });  
+    
+  });
+  
   describe('#compile()', function () {
 
-    it('tests for value and dispays contents', function () {
-      var html = '<p></p>',
-        options = { document: '<a></a>' },
-        reporter = null;
-  
-      var impress = Impress(options, reporter);
-      
-      console.log(impress);
-      
-      impress.compile(html).should.equal('<html></html>');
-    });
-    
     it('should hide block of content is data-imp-test is false', function() {
       
     });
