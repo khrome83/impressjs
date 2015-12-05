f8cbe682b629ad12fc02e6670bddcf97bf8eedd0
Zane Milakovic <zane.milakovic@outlook.com>
2015-12-04 21:16:49 -0600

    Adds ability to accept a reporter

--- test/index.js
+++ test/index.js
@@ -12,12 +12,27 @@ chai.config.truncateThreshold = 0; // disable truncating
 // Impress.js Tests
 describe('impress', function() {
   
-  describe('construction of class', function() {
+  describe('new Impress()', function() {
     
-    it('should construct a instance of Impress with default options if no options are passed in', function() {
-        var impress = new Impress();
-        impress.should.have.deep.property('options');
-    });  
+    it('should return a instance of type Impress()', function() {
+        var imp1 = new Impress();
+        imp1.should.be.an.instanceof(Impress);
+    });
+    
+    it('should return default options if no options are passed in', function() {
+        var imp2 = new Impress();
+        imp2.should.have.deep.property('options');
+    });
+    
+    it('should override default options with any new options passed', function() {
+         var imp3 = new Impress();
+        imp3.should.have.deep.property('options');     
+    });
+    
+    it('should accept argument for reporter', function() {
+         var imp4 = new Impress();
+        imp4.should.have.deep.property('options');     
+    });
     
   });
   
