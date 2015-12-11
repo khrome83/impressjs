017b6aedb3ad2c93e6a575ab8d4378c592c1c996
Zane Milakovic <zane.milakovic@outlook.com>
2015-12-11 13:26:09 -0600

    makes sure that actions.createManifest() returns a valid object

--- test/action.js
+++ test/action.js
@@ -18,20 +18,25 @@ describe('actions', function () {
 
     it('should include all default commands if plugins includes "defaults"', function () {
       var manifest = actions.createManifest(['defaults'], 'data-imp-');
+      manifest.should.be.a('object');
       manifest.should.have.all.keys(default_actions);
     });
 
     it('should only included specified commands if "defaults" is not specified', function () {
       var manifest = actions.createManifest(['list'], 'data-imp-');
+      manifest.should.be.a('object');
       manifest.should.have.all.keys(['data-imp-list']);
     });
 
     it('should include external plugins if name is specified', function () {
-
+      //var manifest = actions.createManifest(['list', 'chai'], 'data-imp-');
+      //manifest.should.be.a('object');
+      //manifest.should.have.all.keys(['data-imp-list', 'data-imp-chai']);
     });
 
     it('should add all actions to manifest with prefix specified', function () {
       var manifest = actions.createManifest(['test'], 'data-crazy-prefix-');
+      manifest.should.be.a('object');
       manifest.should.have.all.keys(['data-crazy-prefix-test']);
     });
 
