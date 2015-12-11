a61e6f31f46d3f2042a863b9a73a81200143f526
Zane Milakovic <zane.milakovic@outlook.com>
2015-12-11 12:54:42 -0600

    stubs out test case for coverage of test

---
+++ test/actions/test.js
@@ -0,0 +1,38 @@
+'use strict';
+
+// Require Modules
+var chai = require('chai'),
+    test = require('../../lib/actions/test');
+ 
+// Chai Settings
+chai.config.includeStack = true; // turn on stack trace
+chai.config.truncateThreshold = 0; // disable truncating
+
+// Action Test Tests
+describe('actions/test', function () {
+
+  describe('constructor()', function () {
+
+    var action = test({}); 
+    
+    it('should return processed DOM tree', function () {
+      action.should.be.a('object');
+    });
+
+  });
+
+  describe('#getProperties()', function () {
+
+    var props = test.getProperties();
+
+    it('should return props object', function () {
+      props.should.be.a('object');
+    });
+
+    it('props required to contain "command" property', function () {
+      props.should.have.all.keys(['command']);
+      props.command.should.be.a('string');
+    });
+
+  });
+});
