55fb4c495580033570e0703ed4bccab19fdbc3c4
Zane Milakovic <zane.milakovic@outlook.com>
2015-12-31 15:51:39 -0600

    updates impress to use common js pattern

--- test/index.js
+++ test/index.js
@@ -1,10 +1,10 @@
 'use strict';
 
 // Require Modules
-var chai = require('chai'),
-    should = chai.should(),
-    rewire = require('rewire'),
-    _ = require('lodash');
+var chai = require('chai');
+var should = chai.should();
+var rewire = require('rewire');
+var _ = require('lodash');
  
 // Chai Settings
 chai.config.includeStack = true; // turn on stack trace
@@ -13,98 +13,96 @@ chai.config.truncateThreshold = 0; // disable truncating
 // Impress.js Tests
 describe('impress', function () {
 
-  var Impress, impress;
+  var impress;
 
   before(function() {
-    Impress = require('../lib/index');
+    impress = rewire('../lib/index');
   });
 
-  describe('new Impress()', function () {
-
-    describe('with no arguments passed in', function () {
-      
-      var default_actions = [];
-      
-      before(function() {
-        var prefix = rewire('../lib/index').__get__('DEFAULT_OPTIONS').prefix,
-            actions = rewire('../lib/actions').__get__('DEFAULT_ACTIONS');
-        
-        default_actions = _.map(actions, function(action) {
-          return prefix + action;
-        });
-        
-        impress = new Impress();
+  describe('no initialization (defaults)', function () {
+    
+    var default_actions = [], defaults;
+    
+    before(function() {
+      defaults = impress.__get__('DEFAULT_OPTIONS');
+    
+      var prefix = defaults.prefix,
+          actions = rewire('../lib/actions').__get__('DEFAULT_ACTIONS');
+    
+      default_actions = _.map(actions, function(action) {
+        return prefix + action;
       });
-
-      it('should return a instance of type Impress()', function () {
-        impress.should.be.an.instanceof(Impress);
-      });
-
-      it('should return default options', function () {
-        impress.should.be.a('object');
-        impress.should.have.property('options');
-        impress.options.should.be.a('object');
-      });
-
-      it('should contain not contaian a reporter', function () {
-        impress.should.be.a('object');
-        impress.should.have.property('reporter');
-        should.not.exist(impress.reporter);
-      });
-
-      it('should return manifest with default actions', function () {
-        impress.should.be.a('object');
-        impress.should.have.property('manifest');
-        impress.manifest.should.be.a('object');
-        impress.manifest.should.have.property('attribute');
-        impress.manifest.attribute.should.be.a('object');
-        impress.manifest.attribute.should.contain.all.keys(default_actions);
-      });
-
+    
     });
 
-    describe('with arguments passed in', function () {
+    it('should return default options', function () {
+      var options = impress.__get__('options');
+    
+      options.should.be.a('object');
+      options.should.eql(defaults);
+    });
 
-      before(function(){
-        var options = {
-              prefix: 'data-i-'
-            },
-            reporter = function Reporter() { },
-            plugins = ['test'];
+    it('should contain not contaian a reporter', function () {
+      var reporter = impress.__get__('reporter');
+    
+      should.not.exist(reporter);
+    });
 
-        impress = new Impress(options, reporter, plugins);
-      });
+    it('should return manifest with default actions', function () {
+      var manifest = impress.__get__('manifest');
+    
+      manifest.should.be.a('object');
+      manifest.should.have.property('attribute');
+      manifest.attribute.should.be.a('object');
+      manifest.attribute.should.contain.all.keys(default_actions);
+    });
 
-      it('should override default options with any new options passed', function () {
-        impress.should.be.a('object');
-        impress.should.have.property('options');
-        impress.options.should.be.a('object');
-        impress.options.should.have.property('prefix');
-        impress.options.prefix.should.equal('data-i-');
-      });
+  });
 
-      it('should accept argument for reporter', function () {
-        impress.should.have.deep.property('reporter');
-        should.exist(impress.reporter);
-      });
+  describe('#init()', function () {
+    var Reporter;
 
-      it('should return manifest with only plugins specified', function () {
-        impress.should.have.property('manifest');
-        impress.manifest.should.be.a('object');
-        impress.manifest.attribute.should.be.a('object');
-        impress.manifest.attribute.should.have.all.keys(['data-i-test']);
-      });
+    before(function(){
+      Reporter = function Reporter() {};
+        
+      var options = {
+            prefix: 'data-i-'
+          },
+          plugins = ['test'];
+      
+      impress.init(options, Reporter, plugins);
+    });
 
+    it('should override default options with any new options passed', function () {      
+      var options = impress.__get__('options');
+    
+      options.should.be.a('object');
+      options.should.have.property('prefix');
+      options.prefix.should.equal('data-i-');
+    });
+
+    it('should accept argument for reporter', function () {
+      var reporter = impress.__get__('reporter');
+      
+      reporter.should.eql(reporter);
+    });
+
+    it('should return manifest with only plugins specified', function () {
+      var manifest = impress.__get__('manifest');
+    
+      manifest.should.be.a('object');
+      manifest.attribute.should.be.a('object');
+      manifest.attribute.should.have.all.keys(['data-i-test']);
     });
 
   });
 
   describe('#compile()', function () {
-    var html, impress;
+    var html;
     
     before(function(){
       html = '<ul class="fruit" id="fruit"><li class="apple" data-active="true">Apple</li><li class="pear">Pear</li><li class="blueberry">Blueberry</li></ul>';
-      impress = new Impress();
+      impress.init();
     });
     
     it('should return html string', function() {
